a01742f25f54e8035dff083973b90fd09837bace
Karabo-Khoza <laurah.karabo@gmail.com>
2025-10-16 10:55:45 +0200

    Added my weather app html and css

--- src/App.js
+++ src/App.js
@@ -1,22 +1,70 @@
 import "./App.css";
-import axios from "axios";
 
 function App() {
-  function handleWeather(response) {
-    alert(
-      `The temperature in Johannesburg is ${response.data.temperature.current}`
-    );
-  }
-
-  let key = "abcf5d3b0bd02t169o09a45ab84f1629";
-  let url = `https://api.shecodes.io/weather/v1/current?query={Johannesburg}&key=${key}`;
-
-  axios.get(url).then(handleWeather);
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World!!!!!</h1>
+    <div class="weather-app">
+      <header>
+        <form class="search-form" id="search-form">
+          <input
+            type="search"
+            placeholder="Enter a city..."
+            required
+            id="search-form-input"
+            class="search-form-input"
+          />
+          <input type="submit" value="Search" class="search-form-button" />
+        </form>
       </header>
+      <main>
+        <div class="weather-app-data">
+          <div>
+            <h1 class="weather-app-city" id="city">
+              Johannesburg
+            </h1>
+            <p class="weather-app-details">
+              <span id="time">Thursday 10:49</span>,
+              <span id="description">partly cloudy</span>
+              <br />
+              Humidity: <strong id="humidity">34%</strong>, Wind:
+              <strong id="wind-speed">12km/h</strong>
+            </p>
+          </div>
+          <div class="weather-app-temperature">
+            <div id="icon">⛅</div>
+            <div class="temperature-value" id="temperature">
+              14
+            </div>
+            <div class="temperature-unit">°C</div>
+          </div>
+        </div>
+        <div class="weather-forecast" id="forecast"></div>
+      </main>
+      <footer>
+        This project was coded by <span> </span>
+        <a
+          href="https://github.com/Karabo-Khoza"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Karabo Khoza
+        </a>
+        , is open-sourced on <span> </span>
+        <a
+          href="https://github.com/Karabo-Khoza/react-practise"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>
+        , and hosted on <span> </span>
+        <a
+          href="https://reactpractis.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+      </footer>
     </div>
   );
 }
